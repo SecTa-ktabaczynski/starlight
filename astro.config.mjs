da450342797a300edccf418e090929f7bf19987d
user <u@s.er>
2026-01-28 10:52:00 +0100

    main-page-sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,18 +11,22 @@ export default defineConfig({
 
     integrations: [starlight({
         title: 'My Docs',
-        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
         sidebar: [
             {
                 label: 'Guides',
                 items: [
                     // Each item here is one entry in the navigation menu.
                     { label: 'Example Guide', slug: 'guides/example' },
+                    { label: 'Docs', slug: 'guides/docs' },
                 ],
             },
             {
                 label: 'Reference',
                 autogenerate: { directory: 'reference' },
+            },
+            {
+                label: 'Custom',
+                autogenerate: { directory: 'custom' },
             },
         ],
 		}), mdx()],
